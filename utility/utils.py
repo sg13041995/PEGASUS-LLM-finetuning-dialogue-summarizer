75971765a94ddffe5cb8dc58b603287c8c6c3477
sg13041995 <subhankar.github.130495@gmail.com>
2024-06-01 17:03:15 +0530

    project documentation updated; code comment updated;

--- utility/utils.py
+++ utility/utils.py
@@ -1,15 +1,17 @@
 device = "cpu"
 
 
+# Function to tokenize the dataset or a batch of data
+
 def convert_examples_to_features(example_batch):
-    # Preprocessing the dialogue
+    # Tokenizing the dialogue or input part of the batch of examples
     input_encodings = tokenizer(example_batch['dialogue'] , max_length = 1024, truncation = True )
 
-    # Preprocessing the summary considering them as target
+    # Tokenizing the corresponding summary or target part of the batch of examples
     with tokenizer.as_target_tokenizer():
         target_encodings = tokenizer(example_batch['summary'], max_length = 128, truncation = True )
 
-    # Returning the preprocessed data as dict which is a requirement
+    # Returning the preprocessed input and target data as dict which is a requirement
     return {
         'input_ids' : input_encodings['input_ids'],
         'attention_mask': input_encodings['attention_mask'],
@@ -17,8 +19,12 @@ def convert_examples_to_features(example_batch):
     }
 
 
+# Function to generate data chunks based on the given batch size
+# Accept dataset[column_text] and dataset[column_summary]
+# Function to be used in 'calculate_metric_on_test_ds' function
+
 def generate_batch_sized_chunks(list_of_elements, batch_size):
-    
+
     """split the dataset into smaller batches that we can process simultaneously
     Yield successive batch-sized chunks from list_of_elements."""
 
@@ -35,12 +41,12 @@ def calculate_metric_on_test_ds(dataset,
                                 column_text="article",
                                 column_summary="highlights"):
 
-    # Generating batches of input and output
+    # Generating batches of input and corresponding output
     article_batches = list(generate_batch_sized_chunks(dataset[column_text], batch_size))
     target_batches = list(generate_batch_sized_chunks(dataset[column_summary], batch_size))
 
     # Iterating through batches
-    # Using tqdm for progress bar 
+    # Using tqdm for progress bar
     for article_batch, target_batch in tqdm(
         # Zipping input and target
         zip(article_batches, target_batches), total=len(article_batches)):
@@ -53,7 +59,7 @@ def calculate_metric_on_test_ds(dataset,
         summaries = model.generate(input_ids=inputs["input_ids"].to(device),
                          attention_mask=inputs["attention_mask"].to(device),
                          length_penalty=0.8, num_beams=8, max_length=128)
-        
+
         ''' parameter for length penalty ensures that the model does not generate sequences that are too long. '''
 
         # Finally, we decode the generated texts, replace the token, and add the decoded texts with the references to the metric.
